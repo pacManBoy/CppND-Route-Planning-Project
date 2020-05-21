d2e87264fc110c48c9aa19e621ddc7e6587dc89b
Pankaj Singh <theiterator@gmail.com>
2020-05-21 12:34:12 +0530

    Updated main method for map1.osm

--- src/main.cpp
+++ src/main.cpp
@@ -11,16 +11,16 @@
 using namespace std::experimental;
 
 static std::optional<std::vector<std::byte>> ReadFile(const std::string &path)
-{   
-    std::ifstream is{path, std::ios::binary | std::ios::ate};
+{
+    std::ifstream is{path, std::ios::binary | std::ios::ate}; // ate moves the pointer to end
     if( !is )
         return std::nullopt;
-    
-    auto size = is.tellg();
-    std::vector<std::byte> contents(size);    
-    
-    is.seekg(0);
-    is.read((char*)contents.data(), size);
+
+    auto size = is.tellg(); // since pointer is at end, gives the complete length
+    std::vector<std::byte> contents(size);
+
+    is.seekg(0); // making pointer to move to front
+    is.read((char*)contents.data(), size); // read data until size (complete length) to contents
 
     if( contents.empty() )
         return std::nullopt;
@@ -28,8 +28,9 @@ static std::optional<std::vector<std::byte>> ReadFile(const std::string &path)
 }
 
 int main(int argc, const char **argv)
-{    
+{
     std::string osm_data_file = "";
+    // First argument is argv[0] holds the name of program
     if( argc > 1 ) {
         for( int i = 1; i < argc; ++i )
             if( std::string_view{argv[i]} == "-f" && ++i < argc )
@@ -40,9 +41,9 @@ int main(int argc, const char **argv)
         std::cout << "Usage: [executable] [-f filename.osm]" << std::endl;
         osm_data_file = "../map.osm";
     }
-    
+
     std::vector<std::byte> osm_data;
- 
+
     if( osm_data.empty() && !osm_data_file.empty() ) {
         std::cout << "Reading OpenStreetMap data from the following file: " <<  osm_data_file << std::endl;
         auto data = ReadFile(osm_data_file);
@@ -51,16 +52,18 @@ int main(int argc, const char **argv)
         else
             osm_data = std::move(*data);
     }
-    
+
     // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
-
+    float start_x, start_y, end_x, end_y;
+    std::cin >> start_x >> start_y >> end_x >> end_y;
+//    start_x = 10, start_y = 10, end_x = 90, end_y = 90;
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
